3586bae82cd739783614ddfd2c56f5fccdbfd913
Vaka-vaka <Waka2006@ukr.net>
2021-11-29 11:21:44 +0200

    Create Main.java

---
+++ home_work/home_work/src/main/java/ua/goit/Main.java
@@ -0,0 +1,15 @@
+/**
+ * Module 5. HTTP (HomeWork)
+ *
+ * @autor Valentin Mozul
+ * @version of 29.11.2021
+ */
+
+package ua.goit;
+
+public class Main {
+
+    public static void main( String[] args ) {
+
+    }
+}
